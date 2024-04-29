291b48d7f45adce0d5421761407646b4c1749c72
Maryann-10 <166915301+Maryann-10@users.noreply.github.com>
2024-04-29 11:02:23 +0300

    Update style.js
    Added API integration

--- style.js
+++ style.js
@@ -51,6 +51,46 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+let apiKey = "bc3f7ca48f5f17t83887o07bc7c6a9af";
+let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+axios(apiURL).then(displayForecast);
+}
+function displayForecast(response){
+console.log(response.data) 
+let forecastHtml = "";
+
+response.data.daily.forEach(function (day, index) {
+  if (index < 5) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      
+      <img src="${day.condition.icon_url}" class="weather-app-icon" />
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature">
+          <strong>${Math.round(day.temperature.maximum)}°</strong>
+        </div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}°</div>
+      </div>
+    </div>
+    `;
+    }
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
